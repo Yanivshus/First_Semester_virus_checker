4130f1f22d537c19d790af837dc5f776d3e2e953
Magshimim <Magshimim@ASUS>
2023-05-21 19:26:46 +0300

    wish done

--- HW8_Yaniv_Shusterman/prog.c
+++ HW8_Yaniv_Shusterman/prog.c
@@ -237,6 +237,7 @@ char* createFilePath(char* nameFile, char* dirPath)
 	if (path == NULL)
 	{
 		printf("erorr mallocating memoy\n");
+		return 1;
 	}
 	strcpy(path, dirPath);
 	strcat(path, SEPERATOR);// putting the path together with /.
@@ -343,6 +344,7 @@ void createLogFile(fileDetails* files, int numFiles, char* pathDir, char* virusS
 	if (logFile == NULL)
 	{
 		printf("problam opening file!");
+		return 1;
 	}
 	fprintf(logFile, "Anti-virus began! Welcome!\n\nFolder to scan: \n%s\nVirus signature: \n%s\n\nScanning option: \n%s\n\nresults: \n", pathDir, virusSignature, scanningOption);
 	for (int i = 0; i < numFiles; i++)
